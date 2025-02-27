c110ce95957c951be4050032e8e317a75458c472
Xwilarg <xwilarg@protonmail.com>
2025-02-27 23:14:55 +0100

    Add website

--- website/index.php
+++ website/index.php
@@ -9,7 +9,14 @@ $loader = new FilesystemLoader(["templates"]);
 $twig = new Environment($loader);
 
 $minipi = json_decode(file_get_contents("data/minipi.json"), true);
+$specials = json_decode(file_get_contents("data/specials.json"), true);
+$speArray = [];
+
+foreach(range('A','Z') as $v){
+    $speArray[$v] = in_array($v, $specials);
+}
 
 echo $twig->render("index.html.twig", [
-    "minipi" => $minipi
+    "minipi" => $minipi,
+    "specials" => $speArray
 ]);
--- website/js/index.js
+++ website/js/index.js
@@ -1,16 +1,26 @@
 function setup()
 {
-    document.getElementById("modal-close").addEventListener("click", _ => {
-        document.getElementById("modal").classList.remove("is-active");
+    document.getElementById("modal-minipi-close").addEventListener("click", _ => {
+        document.getElementById("modal-minipi").classList.remove("is-active");
+    });
+    document.getElementById("modal-specials-close").addEventListener("click", _ => {
+        document.getElementById("modal-specials").classList.remove("is-active");
     });
 
     for (let minipi of document.getElementsByClassName("button-minipi"))
     {
         minipi.addEventListener("click", _ => {
-            document.getElementById("modal-title").innerHTML = minipi.innerHTML;
-            document.getElementById("modal-likes").innerHTML = minipi.dataset.likes.replaceAll("\n", "<br>");
-            document.getElementById("modal-behaviors").innerHTML = minipi.dataset.behaviors.replaceAll("\n", "<br>");
-            document.getElementById("modal").classList.add("is-active");
+            document.getElementById("modal-minipi-title").innerHTML = minipi.innerHTML + (minipi.dataset.writtenby ? ` (${minipi.dataset.writtenby})` : "");
+            document.getElementById("modal-minipi-likes").innerHTML = minipi.dataset.likes.replaceAll("\n", "<br>");
+            document.getElementById("modal-minipi-behaviors").innerHTML = minipi.dataset.behaviors.replaceAll("\n", "<br>").replaceAll("\t", "&nbsp;&nbsp;&nbsp;&nbsp;");
+            document.getElementById("modal-minipi").classList.add("is-active");
+        });
+    }
+    for (let minipi of document.getElementsByClassName("button-special"))
+    {
+        minipi.addEventListener("click", _ => {
+            document.getElementById("modal-specials-title").innerHTML = minipi.innerHTML + (minipi.dataset.writtenby ? ` (${minipi.dataset.writtenby})` : "");
+            document.getElementById("modal-specials").classList.add("is-active");
         });
     }
 }
